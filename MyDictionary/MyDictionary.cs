4d3cca138a2f29de4e0b285a258c227e0c6d27c0
Perihan Süleymanoğlu <47482913+perihansuleymanoglu@users.noreply.github.com>
2021-01-30 09:33:45 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tkeys, Tvalues>
+    {
+        Tkeys[] keys;
+        Tvalues[] values;
+
+        public MyDictionary()
+        {
+            keys = new Tkeys[0];
+            values = new Tvalues[0];
+        }
+
+        public void Add(Tkeys key, Tvalues value)
+        {
+            Tkeys[] tempKeys = keys;
+            Tvalues[] tempValues = values;
+
+            keys = new Tkeys[keys.Length + 1];
+            values = new Tvalues[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public Tkeys[] Keys
+        {
+            get { return keys; }
+        }
+
+        public Tvalues[] Values
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> books = new MyDictionary<string, int>();
+            books.Add("Çalıkuşu", 546);
+            books.Add("Daha", 397);
+
+            Console.WriteLine("Kitap Adı: " + books.Keys[0] + "  --  " + "Sayfa sayısı: " + books.Values[0]);
+            Console.WriteLine("Kitap Adı: " + books.Keys[1] + "  --  " + "Sayfa sayısı: " + books.Values[1]);
+        }
+    }
+}
